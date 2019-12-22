a84d8b95a214ad52038fca979a17b751dae63461
root <ankitvarun.mohanty@gmail.com>
2019-12-22 07:20:35 +0000

    ! in sign up

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,7 @@ router.post("/register", function(req,res){
 			return res.redirect("/register");
 		}
 		passport.authenticate("local")(req,res, function(){
-			req.flash("success", "Welcome to YelpCamp " + user.username);
+			req.flash("success", "Welcome to YelpCamp " + user.username + "!");
 			res.redirect("/campgrounds");
 		});
 	});
@@ -40,7 +40,7 @@ router.get("/login", function(req,res){
 router.post("/login", passport.authenticate("local",
 	{
 		successRedirect: "/campgrounds",
-		failureRedirect: "/login"
+		failureRedirect: "/login",
 	}), function(req,res){
 });
 
